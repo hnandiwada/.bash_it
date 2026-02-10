385fe588f8bd052fe98a88c05d34c2a63450c1b4
Harsha Nandiwada <harsha@trackstarhq.com>
2026-02-10 13:21:16 -0800

    get boto setubs

--- aliases/custom.aliases.bash
+++ aliases/custom.aliases.bash
@@ -21,7 +21,7 @@ alias brewe=brew
 
 alias makee=make
 
-alias venv='deactivate; yarn venv && activate && pip3 install neovim pynvim isort black ruff'
+alias venv='deactivate; yarn venv && activate && pip3 install neovim pynvim isort black ruff boto3-stubs'
 alias activate='source venv/bin/activate'
 alias python='python3'
 
